293aa0ecd937ccf0596802d35473ce98fd574fd2
AhYoungJo <doubletwosep@gmail.com>
2025-08-30 21:59:15 +0900

    feat(client): 컴포넌트 생성 (로직X)

---
+++ src/components/GitHubStats.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function GitHubStats() {
+  return (
+    <div>
+      <h1>GitHub Stats</h1>
+    </div>
+  );
+}
---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ProjectList() {
+  return (
+    <div>
+      <h1>Project List</h1>
+    </div>
+  );
+}
