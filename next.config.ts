047013d824223a2429e03b974a034196f0cfefed
AhYoungJo <doubletwosep@gmail.com>
2025-08-31 15:52:08 +0900

    core: 빌드 시 난독화 on, 웹팩 압축 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+  productionBrowserSourceMaps: false,
+
+  webpack: (config, {dev, isServer}) => {
+    if (!dev && !isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all',
+            },
+          },
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
